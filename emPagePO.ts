96563cba28c8b5b6bac2833278bcb731d4ddb2af
KendraJones <kendralee.jones81@gmail.com>
2022-08-17 12:36:34 -0600

    commiting 2.7.2

--- emPagePO.ts
+++ emPagePO.ts
@@ -1,43 +1,41 @@
-import { Builder, By, Capabilities, until, WebDriver, WebElement } from "selenium-webdriver";
-const chromedriver = require("chromedriver")
+import {Builder, By, Capabilities, WebDriver, until, WebElement} from "selenium-webdriver";
+const chromedriver = require("chromedriver");
 const driver: WebDriver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 export class emPagePO {
     driver: WebDriver;
     url: string = 'https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html'
-    searchBar: By = By.name('q')
-    results: By = By.id('rso')
-    constructor(driver: WebDriver, url: string) {
+    header: By = By.css('.titleText')
+    addEmployee: By = By.css('[name="addEmployee"]')
+    newEmployee: By = By.css('[name="employee11"]')
+    nameField: By = By.css('[name="nameEntry"]')
+    phoneField: By = By.css('[name="phoneEntry"]')
+    titleField: By = By.css('[name="titleEntry"]')
+    saveBtn: By = By.xpath('//button[@id="saveBtn"]')
+    editEmployee: By = By.css('[name="employee1"]')
+    constructor(driver: WebDriver){
         this.driver = driver
-        this.url = url
     }
     async navigate() {
         await this.driver.get(this.url)
-        await this.driver.wait(until.elementLocated(this.searchBar))
+        await this.driver.wait(until.elementLocated(this.header))
     }
-    async getElement(elementBy: By): Promise<WebElement> {
+    async click(elementBy: By) {
         await this.driver.wait(until.elementLocated(elementBy))
-        let element = await this.driver.findElement(elementBy)
-        await this.driver.wait(until.elementIsVisible(element))
-        return element
-    }
-    async setInput(elementBy: By, keys: any): Promise<void> {
-        let input = await this.getElement(elementBy)
-        await input.clear
-        return input.sendKeys(keys)
+        return (await this.driver.findElement(elementBy)).click()
     }
     async sendKeys(elementBy: By, keys) {
         await this.driver.wait(until.elementLocated(elementBy))
-        return driver.findElement(elementBy).sendKeys(keys)
-        }
-    async doSearch(searchTerm: string) {
-        await this.setInput(this.searchBar, `${searchTerm}\n`)
+        return this.driver.findElement(elementBy).sendKeys(keys)
     }
-    async getText(elementBy: By) {
+    async getText(elementBy) {
         await this.driver.wait(until.elementLocated(elementBy))
         return this.driver.findElement(elementBy).getText()
     }
-    async getResults() {
-        return this.getText(this.results)
+    async setInput(elementBy: By, keys: any): Promise<void>{
+        let input = await this.driver.findElement(elementBy)
+        await input.click()
+        await input.clear()
+        return input.sendKeys(keys)
     }
-}
+  }
--- employeeManager.test.ts
+++ employeeManager.test.ts
@@ -1,23 +1,29 @@
-import { emPagePO } from "./emPagePO";
+import { Builder, By, until, Capabilities, WebDriver, WebElement } from "selenium-webdriver";
 const chromedriver = require("chromedriver");
-import { Builder, By, Capabilities, until, WebDriver, WebElement } from "selenium-webdriver";
-
 const driver: WebDriver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
-const page = new emPagePO(driver, "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html");
+import { emPagePO } from "./emPagePO";
 
-test("Create a new employee", async () => {
-  await page.navigate();
-  await page.doSearch("");
-  expect(await page.getResults()).toContain("");
-});
+const employeePage = new emPagePO(driver);
 
-test("Edit an employee", async () => {
-    await page.navigate();
-    await page.doSearch("");
-    expect(await page.getResults()).toContain("");
-  });
-
-afterAll(async () => {
-  await driver.quit();
-});
+describe("Testing Employee Manager", () => {
+    beforeEach(async () => {
+        await employeePage.navigate()
+    })
+    afterAll(async () => {
+        await employeePage.driver.quit()
+    })
+    test('Creating an employee', async () => {
+        await employeePage.click(employeePage.addEmployee)
+        await employeePage.click(employeePage.newEmployee)
+        await employeePage.setInput(employeePage.nameField, "Fake Name")
+        await employeePage.setInput(employeePage.phoneField, "1231231234")
+        await employeePage.setInput(employeePage.titleField, "CEO")
+        await employeePage.click(employeePage.saveBtn)
+    })
+    test('Editing an Employee', async () => {
+      await employeePage.click(employeePage.editEmployee)
+      await employeePage.setInput(employeePage.nameField, "Test Name")
+      await employeePage.click(employeePage.saveBtn)
+    })
+})
